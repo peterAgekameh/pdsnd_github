7761b4a71d30c5c9d87d94b6ee38579c210f52e2
Peter-Osikemeh-Agekameh <osipeter94@gmail.com>
2022-01-14 02:14:20 +0100

    refactor: changes to input field

--- bikeshare (1).py
+++ bikeshare (1).py
@@ -191,7 +191,7 @@ def birth_stat(df):
     mini_year = df['Birth Year']
     earliest_birth = birth_year.min()
     print("The most earliest birth year:{}".format(earliest_birth))
-  
+
 def row_view(df):
     """This is to show the user the five rows of the data set for the respective city,month and day respectively"""
     print("do you want to view 5 rows of data? answer the next question below ")
@@ -214,7 +214,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nTo restart Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
